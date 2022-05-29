36bcbed9c743555c335f753493ec39c6e56d0434
LuizzDutra <luizzidutra@gmail.com>
2022-05-29 17:48:09 -0300

    Criei uma classe de tempo

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from cmath import atanh
 import pygame as pg
 import os, sys, math, time
 
@@ -10,7 +9,6 @@ class Player(pg.sprite.Sprite):
 		self.image = self.sprites[0]
 		self.rect = self.image.get_rect(center = (screen_width/2, screen_height/2))
 		self.speed = 5
-
 
 	def ball_throw(self):
 		return Ball(player.rect.center)
@@ -27,10 +25,7 @@ class Player(pg.sprite.Sprite):
 
 	def update(self):
 		self.move()
-
-
 	
-
 class Ball(pg.sprite.Sprite):
 	def __init__(self, player_pos):
 		super().__init__()
@@ -45,16 +40,32 @@ class Ball(pg.sprite.Sprite):
 		self.time = time.time()
 		self.life_time = 5
 
-	def md(self, n):
-		if n < 0:
-			n *= -1
-		return n
-
 	def update(self):
 		self.rect.x += self.speed * self.xdir
 		self.rect.y += self.speed * self.ydir
 		if time.time() - self.time > self.life_time:
 			self.kill()
+
+
+#Classe de tempo que será usada para calcular o horário e passar os dias
+class Time():
+	def __init__(self):
+		self.timescale = 10000 #Escala de tempo em relação à vida real/ padrão = 60
+		self.start_time = time.time()
+		self.cur_time = 0 #Tempo que passou desde a criação do objeto com a classe
+		self.day = 1 #Use o operador "%" por 7 (ex:day % 7) no index do "week_day" para pegar o dia da semana/ Ex: dado day = 1 -> weekday[day%7] --> "Segunda"
+		self.week_day = {0:"Domingo", 1:"Segunda", 2:"Terca", 3:"Quarta", 4:"Quinta", 5:"Sexta", 6:"Sabado"}
+		self.day_duration = 34200
+	
+	def day_pass_check(self):
+		if self.cur_time > self.day_duration:
+			self.day += 1
+			self.start_time = time.time()
+
+	def update(self):
+		self.day_pass_check()
+		self.cur_time = (time.time() - self.start_time) * self.timescale
+	
 
 pg.init()
 screen_width, screen_height = 1280, 720
@@ -69,6 +80,7 @@ player_group.add(player)
 
 ball_group = pg.sprite.Group()
 
+calendar = Time()
 
 def draw():
 	screen.fill((100,100,100))
@@ -91,6 +103,11 @@ while True:
 
 	player_group.update()
 	ball_group.update()
+	calendar.update()
 	draw()
+
+	#Print Debug
+	print("{:.0f}".format(calendar.cur_time//60))
+	print(calendar.week_day[calendar.day%7])
 
 	clock.tick(60)
