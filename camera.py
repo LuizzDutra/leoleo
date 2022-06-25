380845e0169dc32fed74695bca842721bcbb62da
LuizzDutra <luizzidutra@gmail.com>
2022-06-25 14:25:25 -0300

    a câmera destransiciona

--- camera.py
+++ camera.py
@@ -8,10 +8,11 @@ class Camera():
 		self.t_yoffset = 0
 		self.t_xspeed = 0
 		self.t_yspeed = 0
-		self.t_time = 1
+		self.t_time = 0.2
 		self.t_start = 0
 		self.t_transitioning = False
 		self.in_transition = False
+		self.detransition = False
 		self.xoffset = screen.get_width()/2 - self.focus[0]
 		self.yoffset = screen.get_height()/2 - self.focus[1]
 		self.dt = 0
@@ -24,11 +25,14 @@ class Camera():
 		if self.t_transitioning:
 			self.t_xoffset += self.t_xspeed*self.dt
 			self.t_yoffset += self.t_yspeed*self.dt
-			self.xoffset = screen.get_width()/2 - self.focus[0] + self.t_xoffset
-			self.yoffset = screen.get_height()/2 - self.focus[1] + self.t_yoffset
+			self.xoffset = screen.get_width()/2 - self.st_point[0] + self.t_xoffset
+			self.yoffset = screen.get_height()/2 - self.st_point[1] + self.t_yoffset
 			if pg.time.get_ticks()/1000 - self.t_start > self.t_time:
 				self.t_transitioning = False
-				self.in_transition = True
+				if self.detransition:
+					self.clear_transition()
+				if not self.detransition:
+					self.in_transition = True
 		elif self.in_transition:
 			self.xoffset = screen.get_width()/2 - self.t_point[0]
 			self.yoffset = screen.get_height()/2 - self.t_point[1]
@@ -40,11 +44,21 @@ class Camera():
 	#A transição só soma o offset, note a câmera não irá mudar o foco
 	#O propósito desta função é ser usadas em cutscenes sem a movimentação do player
 	def transition(self, t_point:tuple):
-		self.t_transitioning = True
-		self.t_point = t_point
-		self.t_xspeed = (self.focus[0] - t_point[0])/self.t_time
-		self.t_yspeed = (self.focus[1] - t_point[1])/self.t_time
-		self.t_start = pg.time.get_ticks()/1000
+		if self.in_transition:
+			self.detransition = True
+			self.t_transitioning = True
+			self.t_point = t_point
+			self.t_xspeed = (t_point[0] - self.focus[0])/self.t_time
+			self.t_yspeed = (t_point[1] - self.focus[1])/self.t_time
+			self.t_start = pg.time.get_ticks()/1000
+		else:
+			self.detransition = False
+			self.t_transitioning = True
+			self.t_point = t_point
+			self.st_point = self.focus
+			self.t_xspeed = (self.focus[0] - t_point[0])/self.t_time
+			self.t_yspeed = (self.focus[1] - t_point[1])/self.t_time
+			self.t_start = pg.time.get_ticks()/1000
 
 	def clear_transition(self):
 		self.t_transitioning = False
