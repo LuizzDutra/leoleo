bcaaeeec710ea788e5bf18c60e1aec1049f178fe
LuizzDutra <luizzidutra@gmail.com>
2022-06-28 15:05:15 -0300

    colorkey invés de alpha

--- images.py
+++ images.py
@@ -7,6 +7,7 @@ caption_str = "Leo Leo"
 pg.display.set_caption(caption_str)
 pg.display.set_icon(pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Sprites", "Player", "bob.png")), (32, 32)))
 screen = pg.display.set_mode(screen_res)
+default_colorkey = (0,255,0)
 
 #Barras de energia e vida
 bar_width = 100
@@ -22,13 +23,19 @@ energy_bar.fill((255,255,0))
 
 
 player_image = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Sprites", "Player", "bob.png")), (32, 32)).convert()
+player_image.set_colorkey((0,0,0))
 inv_select = pg.image.load(os.path.join("Assets", "Images", "Hud", "inv_slots.png")).convert()
+inv_select.set_colorkey(default_colorkey)
 inv_slot_selected = pg.image.load(os.path.join("Assets", "Images", "Hud", "inv_slots_selected.png")).convert()
-cursor = pg.image.load(os.path.join("Assets", "Images", "Hud", "cursor.png")).convert_alpha()
+inv_slot_selected.set_colorkey(default_colorkey)
+cursor = pg.image.load(os.path.join("Assets", "Images", "Hud", "cursor.png"))
+cursor.set_colorkey(default_colorkey)
 errorimage = pg.image.load(os.path.join("Assets", "Images", "error.png")).convert()
 manguza = pg.Surface((32,32)).convert()
-bola_papel = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Items", "ball.png")), (16,16)).convert_alpha()
-bola_papel_projetil =  pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Items", "ball.png")), (16,16)).convert_alpha()
+bola_papel = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Items", "ball.png")), (16,16)).convert()
+bola_papel.set_colorkey(default_colorkey)
+bola_papel_projetil =  pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Items", "ball.png")), (16,16)).convert()
+bola_papel_projetil.set_colorkey(default_colorkey)
 wall = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Textures", "wall.png")), (32,32)).convert()
 brick = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Textures", "brick.png")), (32,32)).convert()
 grass =  pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Textures", "grass.png")), (32,32)).convert()
@@ -36,5 +43,6 @@ concrete =  pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "T
 wall_list = [wall, brick]
 ground_list = [grass, concrete]
 door = pg.image.load(os.path.join("Assets", "Images", "Textures", "door.png")).convert()
-chave = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Items", "chave.png")), (48,48)).convert_alpha()
+chave = pg.transform.scale(pg.image.load(os.path.join("Assets", "Images", "Items", "chave.png")), (48,48)).convert()
+chave.set_colorkey(default_colorkey)
 money = pg.image.load(os.path.join("Assets", "Images", "Items", "money.png")).convert()
